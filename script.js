857d577e772944dddb0c84902ae68d86c1942f93
Menagias01 <sdi1900331@di.uoa.gr>
2024-10-15 00:43:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+    return false;
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+    return false;
+}
